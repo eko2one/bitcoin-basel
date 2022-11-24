c32336c19d89af9ce25ffd2f1e775d50db05280a
o3k1n <eknme@protonmail.com>
2022-11-24 20:47:25 +0100

    improve build

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
+rm -rf /build
 rollup -c
 pug ./src/index.pug -o ./build
-mkdir -p ./build/static/fonts
-cp  -R  ./src/fonts/ ./build/static/fonts/
-mkdir -p ./build/static/img
-cp  -R  ./src/img/ ./build/static/img/
+cp -R ./static ./build/
+#mkdir -p ./build/static/fonts
+#cp  -R  ./src/fonts/ ./build/static/fonts/
+#mkdir -p ./build/static/img
+#cp  -R  ./src/img/ ./build/static/img/
